32f7706f17121e51ed99c1e2fc4e1795c246ff75
Alain HUA <ahua@e2r4p17.42.fr>
2017-02-23 15:12:15 +0100

    start

--- server.js
+++ server.js
@@ -3,8 +3,20 @@ let express = require('express');
 let app = express();
 
 
-app.get('/', (request, response) => {
-    response.send('salut');
+app.use('/static', express.static('public'));
+
+app.set('view engine', 'ejs');
+
+
+
+
+app.get('/', (req, res) => {
+    res.render('index', {test: 'salut'});
 })
+
+app.get('/signin', (req, res) => {
+    res.render('signin');
+})
+
 
 app.listen(8081);
